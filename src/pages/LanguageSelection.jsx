a3536b87ce6cee5c42c7a84a1967a8b30d1acf63
Alvaro Solari <AlvaroDSolari19@outlook.com>
2026-01-24 20:42:58 -0500

    Install React-Bootstrap and scaffold language selection

--- src/pages/LanguageSelection.jsx
+++ src/pages/LanguageSelection.jsx
@@ -1,3 +1,15 @@
+import {Button, Card} from 'react-bootstrap';
+
 export default function LanguageSelection() {
-    return <h1>Language Selection</h1>
+    return <div className='d-grid gap-2'>
+
+        <Card className='text-center mb-3'>
+            <Card.Header as='h1' className='display-5 fw-bold'>IAMOOT 2026 - Language Selection</Card.Header>
+        </Card>
+
+        <Button variant='primary'>English</Button>
+        <Button variant='primary'>Español</Button>
+        <Button variant='primary'>Português</Button>
+
+    </div>
 }
